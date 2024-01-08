62415c5d2461d1b11a190009e583cc4c3e3fc4d2
johnmous <johnmous@gmail.com>
2024-01-08 15:04:09 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Retrieva-Augmented Generation 
-A first implementation of a Retrieval-Augmented Generation
+# Building a RAG-based LMM application for querying documentation
+
+This repository is inspired by [this](https://www.anyscale.com/blog/a-comprehensive-guide-for-building-rag-based-llm-applications-part-1) blog post and this [github repo](https://github.com/ray-project/llm-applications).
+
+## App description
+Input of the app is the downloaded material from our target website containing the [Django documentation](https://docs.djangoproject.com/en/5.0/). The following snippet was used:
+```commandline
+export EFS_DIR=/home/imoustakas/llm_playground/RAG/django_docs/
+wget -e robots=off --recursive --no-clobber --page-requisites \
+  --html-extension --convert-links --restrict-file-names=windows \
+  --domains docs.djangoproject.com --no-parent --accept=html \
+  -P $EFS_DIR https://docs.djangoproject.com/en/5.0/
+```
+
--- create_vector_db.py
+++ create_vector_db.py
@@ -1,24 +1,31 @@
-import pypdf
-
-# Open the PDF file in read-binary mode
-with open('/home/imoustakas/llm_playground/RAG/Sohil et al. - 2022 - An introduction to statistical learning with appli.pdf', 'rb') as file:
-    # Create a PDF reader object
-    reader = pypdf.PdfReader(file)
-
-    # Get the total number of pages in the PDF
-    num_pages = len(reader.pages)
-    print(num_pages)
-
-    print(reader.pages[12].extract_text())
+import ray
+import matplotlib.pyplot as plt
+from pathlib import Path
+from rag.data import extract_sections
 
 
-    # # Loop through each page and extract the text
-    # counter = 0
-    # for page in reader.pages:
-    #     text = page.extract_text()
-    #     # Do something with the extracted text
-    #     print(f"Page: {counter}")
-    #     print(text)
-    #     if counter > 10:
-    #         break
-    #     counter += 1
+# Ray dataset
+EFS_DIR = "/home/imoustakas/llm_playground/RAG/django_docs/"
+DOCS_DIR = Path(EFS_DIR, "docs.djangoproject.com/en/5.0/")
+ds = ray.data.from_items([{"path": path} for path in DOCS_DIR.rglob("*.html")
+if not path.is_dir()])
+print(f"{ds.count()} documents")
+
+sample_html_fp = Path(EFS_DIR, "docs.djangoproject.com/en/5.0/faq/contributing/index.html")
+# print(sample_html_fp)
+print(extract_sections({"path": sample_html_fp})[4])
+
+# Extract sections
+sections_ds = ds.flat_map(extract_sections)
+print(sections_ds.count())
+
+section_lengths = []
+for section in sections_ds.take_all():
+    section_lengths.append(len(section["text"]))
+
+# Plot
+plt.figure(figsize=(12, 3))
+plt.plot(section_lengths, marker='o')
+plt.title("Section lengths")
+plt.ylabel("# chars")
+plt.savefig("section_lengths.png")
